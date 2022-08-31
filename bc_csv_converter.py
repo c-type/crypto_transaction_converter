d4650b83e0f7f6ad6a94aa71bf8f6d2f470901f4
ctype <ctypecodes@gmail.com>
2022-08-31 19:26:08 +0200

    click command line interace

--- bc_csv_converter.py
+++ bc_csv_converter.py
@@ -1,14 +1,70 @@
+#!/usr/bin/env python
+
 import pandas as pd
 import pathlib
 import datetime
 import numpy as np 
+import click
 
-PARENT_DIR = pathlib.Path(__file__).resolve().parent
+@click.command()
+@click.option('--in_file', default='./testdata/sample_bitcoin_de_transactions.csv', 
+        help='The path to the input csv file obtained from bitcoin.de')
+@click.option('--out_file', default='./testdata/sample_output.csv', 
+        help='Output path for the converted csv file. If just the filename '
+        'is provided, saves into the directory where this script lives.')
+def convert(in_file: str, out_file: str)->None:
+    """Main conversion function"""
 
-in_file_path = PARENT_DIR / 'testdata/sample_bitcoin_de_transactionsc.csv'
-out_file_path = PARENT_DIR / 'testdata/cointracker_table.csv'
+    PARENT_DIR = pathlib.Path(__file__).resolve().parent
 
-bc_df = pd.read_csv(in_file_path, sep=';')
+    in_file = PARENT_DIR / in_file
+    out_file = PARENT_DIR / out_file
+
+    bc_df = pd.read_csv(in_file, sep=';')
+
+    bc_df = format_cols(bc_df)
+    cointracker_cols = ['Date', 'Received Quantity', 'Received Currency', 'Sent Quantity', 'Sent Currency', 'Fee Amount', 'Fee Currency', 'Tag']
+
+    ct_df = pd.DataFrame(columns=cointracker_cols)
+
+    cointracker_format = '%m/%d/%Y %H:%M:%S'
+    date_series = [dt.strftime(cointracker_format) for dt in bc_df.iloc[:,0]]
+
+    # Handle Dates
+    ct_df['Date'] = date_series
+
+    # Handle Purchases
+    purch_idx = np.where(bc_df['Type'] == 'Purchase')[0]
+    ct_df['Received Quantity'][purch_idx] = bc_df['BTC incl. fee'][purch_idx]
+    ct_df['Received Currency'][purch_idx] = 'BTC'
+    ct_df['Sent Quantity'][purch_idx] = bc_df['amount before fee'][purch_idx]
+    ct_df['Sent Currency'][purch_idx] = 'EUR'
+    ct_df['Fee Amount'][purch_idx] = bc_df['BTC incl. fee'][purch_idx] - bc_df['BTC excl. Bitcoin.de fee'][purch_idx]
+    ct_df['Fee Currency'][purch_idx] = 'BTC'
+
+    # Handle Sales
+    sale_idx = np.where(bc_df['Type'] == 'Sale')[0]
+    ct_df['Received Quantity'][sale_idx] = bc_df['amount before fee'][sale_idx]
+    ct_df['Received Currency'][sale_idx] = 'EUR'
+    ct_df['Sent Quantity'][sale_idx] = bc_df['BTC incl. fee'][sale_idx]
+    ct_df['Sent Currency'][sale_idx] = 'BTC'
+    ct_df['Fee Amount'][sale_idx] = bc_df['amount before fee'][sale_idx] - bc_df['amount after Bitcoin.de-fee'][sale_idx]
+    ct_df['Fee Currency'][sale_idx] = 'EUR'
+
+    # Handle Disbursement
+    disburse_idx = np.where(bc_df['Type'] == 'Disbursement')[0]
+    ct_df['Fee Amount'][disburse_idx] = np.abs(bc_df['Incoming / Outgoing'][disburse_idx+1])
+    ct_df['Fee Currency'][disburse_idx] = 'BTC'
+    ct_df['Sent Quantity'][disburse_idx] = np.abs(bc_df['Incoming / Outgoing'][disburse_idx])
+    ct_df['Sent Currency'][disburse_idx] = 'BTC'
+    ct_df['Tag'][disburse_idx] = 'gift'
+
+    # Drop NAN lines
+    ct_df.drop(index=np.where(ct_df['Fee Currency'].isna())[0], inplace=True)
+
+    # Write CSV table
+    ct_df.to_csv(out_file, index=False)
+
 
 def format_cols(df: pd.DataFrame)->pd.DataFrame:
     """Format assign right data types to columns"""
@@ -19,47 +75,6 @@ def format_cols(df: pd.DataFrame)->pd.DataFrame:
 
     df_formatted.iloc[:,0] = date_series
     return df_formatted
-#    for i, string in enumerate(df_formatted[:,0]):
-      
-bc_df = format_cols(bc_df)
-cointracker_cols = ['Date', 'Received Quantity', 'Received Currency', 'Sent Quantity', 'Sent Currency', 'Fee Amount', 'Fee Currency', 'Tag']
 
-ct_df = pd.DataFrame(columns=cointracker_cols)
-
-cointracker_format = '%m/%d/%Y %H:%M:%S'
-date_series = [dt.strftime(cointracker_format) for dt in bc_df.iloc[:,0]]
-
-# Handle Dates
-ct_df['Date'] = date_series
-
-# Handle Purchases
-purch_idx = np.where(bc_df['Type'] == 'Purchase')[0]
-ct_df['Received Quantity'][purch_idx] = bc_df['BTC incl. fee'][purch_idx]
-ct_df['Received Currency'][purch_idx] = 'BTC'
-ct_df['Sent Quantity'][purch_idx] = bc_df['amount before fee'][purch_idx]
-ct_df['Sent Currency'][purch_idx] = 'EUR'
-ct_df['Fee Amount'][purch_idx] = bc_df['BTC incl. fee'][purch_idx] - bc_df['BTC excl. Bitcoin.de fee'][purch_idx]
-ct_df['Fee Currency'][purch_idx] = 'BTC'
-
-# Handle Sales
-sale_idx = np.where(bc_df['Type'] == 'Sale')[0]
-ct_df['Received Quantity'][sale_idx] = bc_df['amount before fee'][sale_idx]
-ct_df['Received Currency'][sale_idx] = 'EUR'
-ct_df['Sent Quantity'][sale_idx] = bc_df['BTC incl. fee'][sale_idx]
-ct_df['Sent Currency'][sale_idx] = 'BTC'
-ct_df['Fee Amount'][sale_idx] = bc_df['amount before fee'][sale_idx] - bc_df['amount after Bitcoin.de-fee'][sale_idx]
-ct_df['Fee Currency'][sale_idx] = 'EUR'
-
-# Handle Disbursement
-disburse_idx = np.where(bc_df['Type'] == 'Disbursement')[0]
-ct_df['Fee Amount'][disburse_idx] = np.abs(bc_df['Incoming / Outgoing'][disburse_idx+1])
-ct_df['Fee Currency'][disburse_idx] = 'BTC'
-ct_df['Sent Quantity'][disburse_idx] = np.abs(bc_df['Incoming / Outgoing'][disburse_idx])
-ct_df['Sent Currency'][disburse_idx] = 'BTC'
-ct_df['Tag'][disburse_idx] = 'gift'
-
-# Drop NAN lines
-ct_df.drop(index=np.where(ct_df['Fee Currency'].isna())[0], inplace=True)
-
-# Write CSV table
-ct_df.to_csv(out_file_path, index=False)
+if __name__ == '__main__':
+    convert()
